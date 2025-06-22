338861f422b911516115fa40e1af5355f3a41a1c
Victor Pereira <joaovictor.pereira@iebtinnovation.com>
2025-06-22 20:17:16 -0300

    tests: Adiciona tests de unidade e integração para os providers de dados de municipios

--- tests/Unit/BrasilApiMunicipioProviderUnitTest.php
+++ tests/Unit/BrasilApiMunicipioProviderUnitTest.php
@@ -6,16 +6,26 @@ use App\Services\BrasilApiMunicipioProvider;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
-class BrasilApiMunicipioProviderTest extends TestCase
+class BrasilApiMunicipioProviderUnitTest extends TestCase
 {
     public function testGetMunicipiosReturnsCorrectData()
     {
+        Http::fake([
+            'https://brasilapi.com.br/api/ibge/municipios/v1/RS' => Http::response([
+                ['nome' => 'Porto Alegre', 'codigo_ibge' => 4314902],
+                ['nome' => 'Canoas', 'codigo_ibge' => 4304606],
+            ], 200)
+        ]);
+
         $service = new BrasilApiMunicipioProvider();
 
         $result = $service->getMunicipiosByUf('RS');
 
         $this->assertIsArray($result);
-        $this->assertNotEmpty($result);
+        $this->assertCount(2, $result);
+
+        $this->assertEquals('Porto Alegre', $result[0]['name']);
+        $this->assertEquals(4314902, $result[0]['ibge_code']);
     }
 
     public function testGetMunicipiosHandlesWrongUF()
